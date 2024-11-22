26e6f1682d0664c0a6d6e74b5602b7a8b66d18fc
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-11-22 17:38:38 -0500

    change name p1Score to p1Display, same as p2

--- Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
+++ Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
@@ -1,7 +1,11 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const p1Display = document.querySelector('#p1Display')
+const p2Display = document.querySelector('#p1Display')
+
+let p1Score = 0;
 
 p1Button.addEventListener('click', function () {
-    alert('CLICKED')
+    p1Score += 1;
 })
 
