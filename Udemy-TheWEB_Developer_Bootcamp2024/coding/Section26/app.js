d11614c13a3aba7319f14350418bfd976d94e4f7
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-11-22 17:42:29 -0500

    setting winning score

--- Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
+++ Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
@@ -3,7 +3,12 @@ const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p1Display')
 
+
+
 let p1Score = 0;
+// 此处要设置最终分数是多少，这样游戏有结束目标，为什么使用let 不用const，是因为可以选择一个不同的赢分，这样变量的值就会改变
+let winningScore = 5;
+
 
 p1Button.addEventListener('click', function () {
     p1Score += 1;
