989e392cd0c35eb84c852649c6ddad9afd198573
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-11-21 21:13:51 -0500

    updated Section 26

--- Udemy-TheWEB_Developer_Bootcamp2024/coding/Section25/DOM_Events_CODE/TheEventObject/app.js
+++ Udemy-TheWEB_Developer_Bootcamp2024/coding/Section25/DOM_Events_CODE/TheEventObject/app.js
@@ -1,3 +1,4 @@
+// 事件对象Event Object，它会自动传递给回调函数
 document.querySelector('button').addEventListener('click', function (evt) {
     console.log(evt)
 })
@@ -11,6 +12,13 @@ document.querySelector('button').addEventListener('click', function (evt) {
 //     console.log("KEYUP")
 // })
 
+/*
+当你给事件监听器（addEventListener）传递一个回调函数时，浏览器会自动将事件对象传递给这个函数。这个事件对象 e 包含了关于事件发生时的信息，比如按下的键、鼠标的坐标等等。
+在你的例子中，e 是一个 KeyboardEvent 对象，表示键盘事件。它包含许多有用的属性，例如：
+e.code: 表示按键的物理位置（比如 'ArrowUp'、'KeyA' 等）。
+e.key: 表示按键的具体值（比如 'ArrowUp' 或 'a'）。
+其他属性，比如 e.shiftKey（是否按下了 Shift 键）、e.ctrlKey（是否按下了 Ctrl 键）等等。
+*/
 window.addEventListener('keydown', function (e) {
     switch (e.code) {
         case 'ArrowUp':
--- Udemy-TheWEB_Developer_Bootcamp2024/coding/Section25/DOM_Events_CODE/notes.md
+++ Udemy-TheWEB_Developer_Bootcamp2024/coding/Section25/DOM_Events_CODE/notes.md
@@ -23,4 +23,10 @@ btn3.addEventListener('click', function () {
 })
 
 
-Key word ----> this
+2. Key word ----> this
+
+3. The Event object
+4. Form Event 当你输入form的时候会自动跳action 就是这里，这个部分就是后端需要做的，你的url会变成action里写的东西 但可以设置成只在console里提示 .preventDefault（）可以避免跳转
+    Form event文件里有实例
+
+
