35abc28bf48314c4ec3cb3c7cae0aa6c002c425b
xiaoxy <xiaoxiaoyu0226@gmail.com>
2024-11-22 17:47:12 -0500

    adding p2 score for display

--- Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
+++ Udemy-TheWEB_Developer_Bootcamp2024/coding/Section26/app.js
@@ -1,18 +1,32 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p1Display')
+const p2Display = document.querySelector('#p2Display')
 
 
 
 let p1Score = 0;
 // 此处要设置最终分数是多少，这样游戏有结束目标，为什么使用let 不用const，是因为可以选择一个不同的赢分，这样变量的值就会改变
 let winningScore = 5;
-
+let p2Score = 0; //先初始化，这样就可以记录分数了
 
 p1Button.addEventListener('click', function () {
-    p1Score += 1;
-    // 这个是当你点击p1Button，p1Display就会相应增加
-    p1Display.textContent = p1Score;
+    // 如果p1的成绩没有达到winningscore，就接着网上加
+    if (p1Score !== winningScore) {
+        p1Score += 1;
+        // 这个是当你点击p1Button，p1Display就会相应增加
+        p1Display.textContent = p1Score;
+    }
+
+})
+
+p2Button.addEventListener('click', function () {
+    // 如果p1的成绩没有达到winningscore，就接着网上加
+    if (p2Score !== winningScore) {
+        p2Score += 1;
+        // 这个是当你点击p1Button，p1Display就会相应增加
+        p2Display.textContent = p2Score;
+    }
+
 })
 
