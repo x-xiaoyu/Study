5e9b477c40bf4ded8563603177acadaa046dc8dc
xiaoxy <xiaoxiaoyu0226@gmail.com>
2025-08-22 18:26:13 -0400

    updated

---
+++ 04 HeiMa_Vue2Vue3/Day3/05-脚手架相关代码/project/src/demo1练习/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 这个类名最好和组件名同名 -->
+  <div class="App">
+    <!-- 头部组件 -->
+    <HmHeader></HmHeader>
+    <!-- 主体组件 -->
+    <HmMain></HmMain>
+    <!-- 底部组件 -->
+    <HmFooter></HmFooter>
+    
+
+  </div>
+</template>
+
+<script>
+import HmHeader from './components/HmHeader.vue'
+import HmMain from './components/HmMain.vue'
+import HmFooter from './components/HmFooter.vue'
+
+export default {
+  components:{
+    //'组件名'：组件对象 可以直接写名字 也可以全部都写出来
+    HmHeader:HmHeader,
+    // HmMain:HmMain,
+    // HmFooter:HmFooter
+    HmMain,
+    HmFooter
+  }
+}
+</script>
+
+<style>
+.App{
+  width: 600px;
+  height: 700px;
+  background-color: #87ceeb;
+  margin:0 auto;
+  padding: 20px;
+}
+</style>
--- 04 HeiMa_Vue2Vue3/Day3/05-脚手架相关代码/project/src/main.js
+++ 04 HeiMa_Vue2Vue3/Day3/05-脚手架相关代码/project/src/main.js
@@ -1,28 +1,12 @@
-// 此文件的核心作用： 1. 导入app.vue 基于app.vue 创建解构渲染index.html
-
-// 1. 导入vue核心包
 import Vue from 'vue'
-
-// 2， 导入vue.app 根组件
 import App from './App.vue'
+import './styles/base.css' // css 样式重置
+import './styles/common.css' // 公共全局样式
+import './assets/iconfont/iconfont.css' // 字体图标的样式
 
-// 编写导入的代码， 王代码的顶部编写（规范） 不加.vue也是可以用的
-import HmButton from './components/HmButton'
-
-// 提示：当前从处于什么环境（生产环境/开发环境）false什么提示都不显示 true就会显示
+import './styles/index.css' // 页面样式
 Vue.config.productionTip = false
 
-// 进行全局注册 ->在所有的组件范围内都能适用
-// Vue.component(组件名，组件对象)
-Vue.component('HmButton', HmButton)
-
-// 3. vue实例化，提供render方法 作用是基于app.vue 创建解构渲染index.html 
 new Vue({
-  // el:'#app', 作用：.$mount('选择器')作用一直，用于指定vue所管理容器
-  // render: h => h(App),
-  // render完整写法 h换成了createElement
-  render: (createElement) =>{
-    // 基于App创建元素结构 这个结构用于以后渲染index
-    return createElement(App)
-  }
+  render: h => h(App),
 }).$mount('#app')
